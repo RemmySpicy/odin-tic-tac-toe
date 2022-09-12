72e85a470923f0c7be8032c75bd7b811bf90863f
O.Remilekun <remmylknolaotan@gmail.com>
2022-09-12 21:56:09 +0100

    Add basic UI

---
+++ script.js
@@ -0,0 +1,47 @@
+(gameBoard = () => {
+    let board = ['x', 'o', 'x', 'o', 'x', 'o', 'x', 'o', 'x'];
+    console.log(board)
+})()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// (displayController => {
+
+// })()
+
+// console.log(board)
+
+function Player(name) {
+    player = (name) => {
+        return 'Player' + name;
+    } 
+}
+
+playerOne = {
+
+}
+
+playerTwo = {
+
+}
+
+function play () {
+    let array = []
+    // array
+}
+
+// Build the functions that allow players to add marks to a specific spot on the board, and then tie it to the DOM, letting players click on the gameboard to place their marker. Don’t forget the logic that keeps players from playing in spots that are already taken!
