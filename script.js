f9bd1bfe220dd008227c859cf2ddf1126e71a16e
O.Remilekun <remmylknolaotan@gmail.com>
2022-09-15 22:33:22 +0100

    Add win pattern array or arrrays

--- script.js
+++ script.js
@@ -12,8 +12,15 @@ reset.addEventListener('click', resetGame);
 // Gameboard data
 let gameBoard = [];
 
-let winPattern = [
-    []
+let winPatterns = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
 ]
 
 // (displayController => {
